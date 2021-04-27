121bb17149d47e9fc786a175c2e41adf85a3f132
Radhasyam <radhasyamnunna@gmail.com>
2021-04-27 06:19:45 +0530

    Message

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+
+void main()  {
+  // print('hello');
+  print('hiiiiiiiiii');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: new Text("Testing Git", textDirection: TextDirection.ltr,
+    style: TextStyle(
+      color: Colors.cyanAccent,
+    ),
+    ));
+
+  }
+}
